bc4a379e90bfbcb5f5fa619a5d1cd5137e7ac764
HikariLan <shaokeyibb@126.com>
2022-08-17 21:50:38 +0800

    更新测试用例，添加 sprint 和 test5g

--- hello.he.kts
+++ hello.he.kts
@@ -1,5 +1,14 @@
+val array:u8 = 4 `|` 6 `|` 8
+// 4 | 8
+println(array[1 `|` 3])
+
+// Hello, world!
+sprint(72 `|` 101 `|` 108 `|` 108 `|` 111 `|` 44 `|` 32 `|` 119 `|` 111 `|` 114 `|` 108 `|` 100 `|` 33)
+
 val forceCon: u8 = createU8(68)
 
 forceCon[1 `|` 2 `|` 6 `|` 7 `|` 11 `|` 52 `|` 57 `|` 58 `|` 65] = 10
 
-print(forceCon)
+println(forceCon)
+
+test5g()
