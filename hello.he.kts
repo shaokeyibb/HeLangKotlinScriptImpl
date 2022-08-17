f2fd40241777451722362e87cabfe303a8825463
HikariLan <shaokeyibb@126.com>
2022-08-17 22:17:59 +0800

    format

--- hello.he.kts
+++ hello.he.kts
@@ -1,4 +1,4 @@
-var array:u8 = 4 `|` 6 `|` 8
+var array: u8 = 4 `|` 6 `|` 8
 array = ++array
 // 5 | 9
 println(array[1 `|` 3])
@@ -7,9 +7,7 @@ println(array[1 `|` 3])
 sprint(72 `|` 101 `|` 108 `|` 108 `|` 111 `|` 44 `|` 32 `|` 119 `|` 111 `|` 114 `|` 108 `|` 100 `|` 33)
 
 val forceCon: u8 = createU8(68)
-
 forceCon[1 `|` 2 `|` 6 `|` 7 `|` 11 `|` 52 `|` 57 `|` 58 `|` 65] = 10
-
 println(forceCon)
 
 test5g()
